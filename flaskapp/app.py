b0d947e77f8fd1d37c82be7b50f28836dab8d494
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2024-08-15 01:45:47 +0700

    update
    Co-Authored-By: aldif1 <122974954+aldif1@users.noreply.github.com>

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -5,6 +5,29 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/tegal")
+def index():
+    return render_template("index.html")
+@app.route("/semarang")
+def index():
+    return render_template("index.html")
+@app.route("/tegal/pai")
+def index():
+    return render_template("index.html")
+@app.route("/semarang/sam_po_kong")
+def index():
+    return render_template("index.html")
+@app.route("/semarang/kota_lama")
+def index():
+    return render_template("index.html")
+@app.route("/semarang/goa_kreo")
+def semarang_goa_kreo():
+    return render_template("goa_kreo.html")
+@app.route("/semarang/goa_kreo/vt")
+def semarang_goa_kreo_vt():
+    return render_template("goa_kreo/index.html")
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    return render_template("admin/dashboard.html")
 if __name__ == "__main__":
     app.run(debug=True)
