b800c529dc807c79f7505455b3ed7cbfa1062cc3
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2024-08-15 03:18:00 +0700

    update
    Co-Authored-By: aldif1 <122974954+aldif1@users.noreply.github.com>

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,session,send_from_directory
+import os
 app = Flask(__name__)
+app.secret_key = 'c~maeri2394i9302/;0a1'  
+project_root = os.path.dirname(os.path.abspath(__file__))
+parent_root = os.path.dirname(project_root)
 
+@app.route('/custom_static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(parent_root, filename)
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/tegal")
-def index():
+def tegal():
     return render_template("index.html")
 @app.route("/semarang")
-def index():
+def semarang():
     return render_template("index.html")
 @app.route("/tegal/pai")
-def index():
+def tegal_pai():
     return render_template("index.html")
 @app.route("/semarang/sam_po_kong")
-def index():
+def semarang_sam_po_kong():
     return render_template("index.html")
 @app.route("/semarang/kota_lama")
-def index():
+def semarang_kota_lama():
     return render_template("index.html")
 @app.route("/semarang/goa_kreo")
 def semarang_goa_kreo():
